6aea05f83b5fc5fc2c20177ca8706d3efb075fd0
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-28 00:26:56 -0300

    Added comments for better understanding

--- rest-with-spring-boot-java/src/main/java/br/com/restspringboot/GreetingController.java
+++ rest-with-spring-boot-java/src/main/java/br/com/restspringboot/GreetingController.java
@@ -13,6 +13,7 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping("/greeting")
+	//@RequestParam is just like the [FromQuery] from .NET it is for query params and has a default value
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
