4651de629314e777d40e19fdaa6548ff01f89664
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-02 15:36:30 -0300

    Update in PersonRepository

--- rest-with-spring-boot-java/src/main/java/br/com/restspringboot/repositories/PersonRepository.java
+++ rest-with-spring-boot-java/src/main/java/br/com/restspringboot/repositories/PersonRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.restspringboot.models.Person;
 
-public interface PersonRepository extends JpaRepository<Person,Long>{}
+public interface PersonRepository extends JpaRepository<Person, Long> {}
