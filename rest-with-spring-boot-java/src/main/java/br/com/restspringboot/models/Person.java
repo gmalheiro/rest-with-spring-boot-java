35c2f65ac6a472adbf896e9406645c875131d219
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-02 14:02:40 -0300

    Update in Person.java added anotations

--- rest-with-spring-boot-java/src/main/java/br/com/restspringboot/models/Person.java
+++ rest-with-spring-boot-java/src/main/java/br/com/restspringboot/models/Person.java
@@ -2,14 +2,29 @@ package br.com.restspringboot.models;
 
 import java.io.Serializable;
 
-public class Person implements Serializable{
-    
-    private static final long serialVersionUID = 1L;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+public class Person implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "first_name", nullable = false, length = 80)
 	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 80)
 	private String lastName;
+	
+	@Column(nullable = false, length = 100)
 	private String address;
+	
+	@Column(nullable = false, length = 6)
 	private String gender;
 	
 	public Person() {}
