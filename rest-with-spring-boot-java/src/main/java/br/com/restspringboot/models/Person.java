7fbbadfb274ea0712c71ae4af0b35cc0ac002d12
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-02 15:27:03 -0300

    Added Table annotation

--- rest-with-spring-boot-java/src/main/java/br/com/restspringboot/models/Person.java
+++ rest-with-spring-boot-java/src/main/java/br/com/restspringboot/models/Person.java
@@ -3,10 +3,14 @@ package br.com.restspringboot.models;
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable{
 
 	private static final long serialVersionUID = 1L;
