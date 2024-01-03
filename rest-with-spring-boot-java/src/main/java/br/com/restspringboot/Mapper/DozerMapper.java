7e22c0142e2f1df1dd32a59010d17bf199c32643
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-03 08:46:50 -0300

    Update in DozerMapper

--- rest-with-spring-boot-java/src/main/java/br/com/restspringboot/Mapper/DozerMapper.java
+++ rest-with-spring-boot-java/src/main/java/br/com/restspringboot/Mapper/DozerMapper.java
@@ -10,7 +10,6 @@ public class DozerMapper {
     
 	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 	
-	// private static ModelMapper mapper = new ModelMapper();
 	
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
